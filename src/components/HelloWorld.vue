4d71ccc38ae539b685f8b08b6d3a26d564bc0032
huchaomin <iisa_peter@163.com>
2023-06-24 12:33:33 +0800

    Revert "Revert "添加stylelint和sass""
    This reverts commit 647f85c47a2b563595331d12a0129b6c7bf9939f.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,17 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="title">{{ msg }}</h1>
     <button type="button" @click="count++">count is {{ count }}</button>
 </template>
+
+<style lang="scss" scoped>
+  .title{
+    position: relative;
+    top: 50%;
+    color: $primary;
+    font-weight: bold;
+    font-size: 24px;
+    font-family: 'Courier New', Courier, monospace;
+  }
+</style>
