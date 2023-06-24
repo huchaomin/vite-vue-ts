647f85c47a2b563595331d12a0129b6c7bf9939f
huchaomin <iisa_peter@163.com>
2023-06-24 12:32:59 +0800

    Revert "添加stylelint和sass"
    This reverts commit 580669d10dd7d9ec8487f8ebe2ea6aac855aab50.

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,12 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import checker from 'vite-plugin-checker'
 import eslint from 'vite-plugin-eslint'
-import stylelint from 'vite-plugin-stylelint'
-
-const resolvePath = (p: string): string => path.resolve(__dirname, p)
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolvePath('src'),
-      img: resolvePath('src/assets/images')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/assets/css/_variable.scss" as *;'
-      }
-    }
-  },
   plugins: [
     vue(),
-    eslint({
-      cache: true,
-      failOnWarning: true
-    }),
-    stylelint({ build: true }),
+    eslint(),
     checker({ vueTsc: true })
   ]
 })
