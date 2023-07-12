0fea0c1fe9188fd71dcc4b34a8c9242e9f493345
huchaomin <iisa_peter@163.com>
2023-07-12 15:32:09 +0800

    解决控制台警告 you should use the "customSyntax" option when

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -2,7 +2,15 @@ module.exports = {
   extends: [
     'stylelint-config-standard-less',
     'stylelint-config-recess-order',
-    'stylelint-config-html', // The shareable HTML (and HTML-like) config for Stylelint.
+    'stylelint-config-recommended-vue',
+  ],
+  // 解决控制台警告 you should use the "customSyntax" option when linting something other than CSS
+  // scss 好像不需要，stylelint-config-standard-less 这个包不包含 postcss-less
+  overrides: [
+    {
+      files: ['**/*.less'],
+      customSyntax: 'postcss-less',
+    },
   ],
   rules: {},
 };
